d0dc899f4f3d2512887528890d029aacf5f0f61c
Katia Nartovich <narto004@d.umn.edu>
2017-03-03 21:19:29 -0600

    Added presenter to the MainActivity.

--- app/src/main/java/com/example/alextangen/unitedpipeline/MainActivity.java
+++ app/src/main/java/com/example/alextangen/unitedpipeline/MainActivity.java
@@ -6,10 +6,14 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Presenter presenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        presenter = new Presenter(this);
     }
 
     public void continueAsClient(View view) {
