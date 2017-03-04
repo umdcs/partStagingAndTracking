51898b1feb358bf8a3b41b5cdffdec6bb54e3fd8
Katia Nartovich <narto004@d.umn.edu>
2017-03-03 20:14:01 -0600

    Added attributes and general description of needed functions to the Piece class.

--- app/src/main/java/com/example/alextangen/unitedpipeline/Piece.java
+++ app/src/main/java/com/example/alextangen/unitedpipeline/Piece.java
@@ -5,4 +5,30 @@ package com.example.alextangen.unitedpipeline;
  */
 
 public class Piece {
+
+    private boolean matlRcvd;
+    private boolean startFab;
+    private boolean endFab;
+    private boolean xRay;
+    private boolean startCoat;
+    private boolean endCoat;
+    private boolean shipRdy;
+
+    private double pfHours;
+    private double lbHours;
+
+    public Piece() {
+        matlRcvd = false;
+        startFab = false;
+        endFab = false;
+        xRay = false;
+        startCoat = false;
+        endCoat = false;
+        shipRdy = false;
+
+        pfHours = 0;
+        lbHours = 0;
+    }
+
+    // public methods to update/access progress and man hours
 }
