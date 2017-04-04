917bfe13808c3e4531fe22c32d907e6f2a828585
Katia Nartovich <narto004@d.umn.edu>
2017-04-04 12:03:25 -0500

    Gave the app permission to use the internet (oops).

--- app/src/main/java/com/example/alextangen/unitedpipeline/NewJob.java
+++ app/src/main/java/com/example/alextangen/unitedpipeline/NewJob.java
@@ -3,6 +3,7 @@ package com.example.alextangen.unitedpipeline;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,7 +28,7 @@ public class NewJob extends AppCompatActivity {
     public void nextActivity(View view) {
 
         RequestManager reqMan = new RequestManager();
-        reqMan.addJob(new Job(1));
+        reqMan.addJob(new Job(2));
 
         Intent intent = new Intent(this, JobProgress.class);
 
--- app/src/main/java/com/example/alextangen/unitedpipeline/RequestManager.java
+++ app/src/main/java/com/example/alextangen/unitedpipeline/RequestManager.java
@@ -1,6 +1,7 @@
 package com.example.alextangen.unitedpipeline;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.google.gson.Gson;
 
@@ -42,6 +43,7 @@ public class RequestManager {
         JSONObject jsonJob = null;
         Gson gson = new Gson();
         String jobString = gson.toJson(job);
+        Log.d("DEBUG", jobString);
         try {
             jsonJob = new JSONObject();
             jsonJob.put("Job", jobString);
@@ -52,53 +54,55 @@ public class RequestManager {
     }
 
     /** Handles HTTP messages by using a separate thread */
-    private class HTTPAsyncTask extends AsyncTask<String, Integer,String> {
+    private class HTTPAsyncTask extends AsyncTask<String, Integer, String> {
 
         @Override
-        /**
+        /*
          * @param params[0] URI
          * @param params[1] type of request (GET or POST)
          * @param params[2] JSON object (only in the case of a POST request)
          * @return string containing contents of HTTP response
-         * @exception java.net.MalformedURLException
-         * @exception java.io.IOException
+         * @exception MalformedURLException
+         * @exception IOException
          */
         protected String doInBackground(String... params) {
 
-            /** Variables for creating a connection to HTTP server */
+            /* Variables for creating a connection to HTTP server */
             HttpURLConnection serverConnection = null;
             InputStream is = null;
 
             try {
                 URL url = new URL(params[0]);
 
-                /** Open connection */
+                /* Open connection */
                 serverConnection = (HttpURLConnection) url.openConnection();
 
-                /** Set GET or POST */
+                /* Set GET or POST */
                 serverConnection.setRequestMethod(params[1]);
 
-                /** In the case of a POST request ... */
+                /* In the case of a POST request ... */
                 if (params[1].equals("POST")) {
+                    Log.d("DEBUG POST", "In post: params[0]=" + params[0] + ", params[1]=" + params[1] + ", params[2]=" + params[2]);
 
-                    /** Prepare connection to handle JSON object */
+                    /* Prepare connection to handle JSON object */
                     serverConnection.setDoOutput(true);
-                    serverConnection.setRequestProperty("Content-Type", "application/json; carset=utf-8");
-                    serverConnection.setRequestProperty("Content-Length", "" + Integer.toString(params[2].toString().getBytes().length));
+                    serverConnection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+                    Log.d("DEBUG", Integer.toString(params[2].getBytes().length));
+                    serverConnection.setRequestProperty("Content-Length", "" + Integer.toString(params[2].getBytes().length));
 
-                    /** JSON data is written to the server */
+                    /* JSON data is written to the server */
                     DataOutputStream out = new DataOutputStream(serverConnection.getOutputStream());
-                    out.writeBytes(params[2].toString());
+                    out.writeBytes(params[2]);
 
-                    /** Flush & close output stream buffer */
+                    /* Flush & close output stream buffer */
                     out.flush();
                     out.close();
                 }
 
-                /** Get HTTP response */
+                /* Get HTTP response */
                 is = serverConnection.getInputStream();
 
-                /** Turn response (including JSON data) into a string */
+                /* Turn response (including JSON data) into a string */
                 StringBuilder sb = new StringBuilder();
                 String line;
                 BufferedReader br = new BufferedReader(new InputStreamReader(is));
@@ -114,6 +118,7 @@ public class RequestManager {
                 e.printStackTrace();
                 return null;
             } finally {
+                Log.d("DEBUG", "Made it this far ...");
                 serverConnection.disconnect();
             }
         }
@@ -125,7 +130,7 @@ public class RequestManager {
          */
         protected void onPostExecute(String result) {
             try {
-                /** Take resulting string from doInBackground & extract JSON object */
+                /* Take resulting string from doInBackground & extract JSON object */
                 JSONObject jsonData = new JSONObject(result);
             } catch (JSONException e) {
                 e.printStackTrace();
