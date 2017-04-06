06673947d97e9c493811e574a53f1041f54b56b2
Ethan Swenson <swens963@d.umn.edu>
2017-04-05 23:07:54 -0500

    Changed a variable name.

--- UPI_node_server.js
+++ UPI_node_server.js
@@ -16,62 +16,67 @@ var app = express();
 app.set("port", 8090);
 
 app.use(bodyParser.urlencoded({ // support encoded bodies
-  extended: true
+    extended: true
 }));
 
- app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.json()); // support json encoded bodies
 
- app.get('/', function(request, response) {
-   response.writeHead(200, {'Content-Type': 'text/html'});
+app.get('/', function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    
+    response.write('<!DOCTYPE html><head><title>Basic Manager Dashboard</title></head><body>');
+    response.write('<H1>Manager Dashboard</H1>');
+    response.write('JSON Data:');
+    
+    /* You could output any JavaScript data here... */
+    response.write(JSON.stringify(jobsArray));
+    response.write('</body></html>');
+    
+    response.end();
+   
+    console.log('Received dashboard request!');
+});
 
-   response.write('<!DOCTYPE html><head><title>Basic Manager Dashboard</title></head><body>');
-   response.write('<H1>Manager Dashboard</H1>');
-   response.write('')
-   response.end();
+app.get('/getAllJobs', function(request, response) {
+    response.send(jobsArray);
+    console.log('Received a get request for all jobs!');
+    response.end();
+});
 
-   console.log('Received dashboard request!');
- });
+app.get('/getJobByID', function(request, response) {
+    //An ID should be sent up with request, so a specific job can be grabbed
+    //Then send back the job corresponding to the ID
+    response.send(jobsArray[0]); //0 for now, will search for job ID later
+    console.log('Received a get request for a job by ID!');
+    response.end();
+});
 
- app.get('/getAllJobs', function(request, response) {
-   response.send(jobsArray);
-   console.log('Received a get request for all jobs!');
-   response.end();
- });
-
- app.get('/getJobByID', function(request, response) {
-   //An ID should be sent up with request, so a specific job can be grabbed
-   //Then send back the job corresponding to the ID
-   response.send(jobsArray[0]); //0 for now, will search for job ID later
-   console.log('Received a get request for a job by ID!');
-   response.end();
- });
-
- app.post('/addJob', function(request, response) {
-   if(!request.body) return res.sendStatus(400);
-
-   var aJob = request.body.job;
-   jobsArray.push(aJob);
-
-   console.log('Received a post request for adding a job!');
-   response.end();
- });
+app.post('/addJob', function(request, response) {
+    if(!request.body) return res.sendStatus(400);
+    
+    var aJob = request.body.job;
+    jobsArray.push(aJob);
+    
+    console.log('Received a post request for adding a job!');
+    response.end();
+});
 
 //To be manager only  
 app.get('/getJobHours', function(request, response) {
-  response.send(jobHours[0]); //0 is placeholder for jobID;
-  console.log('Recieved a get request for man hours!');
-  response.end();
+    response.send(jobHours[0]); //0 is placeholder for jobID;
+    console.log('Recieved a get request for man hours!');
+    response.end();
 });
 
- app.use(function(req, res, next) {
-   res.status(404).send("Sorry can't find that!");
- });
+app.use(function(req, res, next) {
+    res.status(404).send("Sorry can't find that!");
+});
 
- app.use(function(err, req, res, next) {
-   console.error(err.stack);
-   res.status(500).send("Something broke!");
- });
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+});
 
 app.listen(app.get("port"), function () {
-	console.log('UPI Node Server: Node app listening on port: ', app.get("port"));
+    console.log('UPI Node Server: Node app listening on port: ', app.get("port"));
 });
