7df1b4e7ad9497089746fd84450f5c1739192ed4
Katia Nartovich <narto004@d.umn.edu>
2017-04-12 18:32:13 -0500

    Completed the pieceCreator test.

--- app/src/test/java/com/example/alextangen/unitedpipeline/JobUnitTest.java
+++ app/src/test/java/com/example/alextangen/unitedpipeline/JobUnitTest.java
@@ -13,6 +13,7 @@ public class JobUnitTest {
     @Test
     public void jobCreator() {
         Job testJob = new Job(3); // create a new job with 3 pieces
+
         assertThat(testJob.getName(), is(""));
         assertThat(testJob.getID(), is(0));
         assertThat(testJob.getNumPieces(), is(3));
@@ -20,7 +21,8 @@ public class JobUnitTest {
 
     @Test
     public void pieceCreator() {
-        Piece testPiece = new Piece();
+        Piece testPiece = new Piece(); // create a new piece
+
         assertThat(testPiece.getMatlReceived(), is(false));
         assertThat(testPiece.getStartFab(), is(false));
         assertThat(testPiece.getEndFab(),is(false));
@@ -28,5 +30,8 @@ public class JobUnitTest {
         assertThat(testPiece.getStartCoat(), is(false));
         assertThat(testPiece.getEndCoat(), is(false));
         assertThat(testPiece.getShipRdy(), is(false));
+
+        assertThat(testPiece.getPfHours(), is(0.0));
+        assertThat(testPiece.getLbHours(), is(0.0));
     }
 }
