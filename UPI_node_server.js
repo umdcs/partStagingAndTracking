b6a6381a96db20d8e87fecd6abd206bbc4c979ac
Alex Tangen <tange045@d.umn.edu>
2017-04-12 22:54:44 -0500

    Began working on /addJob and /importantInfo screens, both are displaying good progress. Now need to work on getting jobs and converting them back into objects on the android side

--- UPI_node_server.js
+++ UPI_node_server.js
@@ -4,6 +4,8 @@
 
 var jobsArray = [];
 
+var jobsInfoArray = [];
+
 var express = require('express');
 
 var bodyParser = require('body-parser');
@@ -26,7 +28,7 @@ app.get('/', function(request, response) {
     response.write('JSON Data:');
     
     /* You could output any JavaScript data here... */
-    response.write(JSON.stringify(jobsArray));
+    response.write(JSON.stringify(jobsArray, null, 4));
     response.write('</body></html>');
     
     response.end();
@@ -52,12 +54,14 @@ app.get('/getJobByID', function(request, response) {
    the Job names. This will be used to display the current jobs
    that are on the server. Then as the user wants to update or view
    specific jobs, will the job be requested from the server */
+
+/*
 app.get('/getJobInfo', function(request, response) {
     var jobsInfoArray = [];
     var jobInfo;
     
     for(var i = 0; i < jobsArray.length; ++i) {
-	jobInfo = { jobID: "", jobName: "" };
+	jobInfo = { "jobID" : "", "jobName" : "" };
 	jobsInfoArray.push[jobInfo];
     }
 
@@ -65,6 +69,7 @@ app.get('/getJobInfo', function(request, response) {
     console.log('Received a get request for the names of jobs!');
     response.end();
 });
+*/
 
 app.post('/addJob', function(request, response) {
     if(!request.body) return res.sendStatus(400);
@@ -76,9 +81,43 @@ app.post('/addJob', function(request, response) {
     response.end();
 });
 
+
 /* updateJob will be sent a Job to update, and an index
    where this job is located (to be updated). From this, 
    the job can be updated easily without touching other data */
+
+app.post('/importantInfo', function(request,response) {
+    if(!request.body) return response.sendStatus(400);
+
+    //var aJob = request.body.Job;
+
+    var info = {"name" : request.body.name, "ID" : request.body.ID}
+    jobsInfoArray.push(info);
+
+    response.write("Success");
+    console.log('Pushed Name and ID onto jobsInfoArray');
+    response.end();
+});
+
+app.get('/getImportantInfo', function(request,response) {
+    
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    
+    response.write('<!DOCTYPE html><head><title>Job Information</title></head><body>');
+    response.write('<H1>Names and Numbers</H1>');
+    response.write('JSON Data:');
+    
+    /* You could output any JavaScript data here... */
+    response.write(JSON.stringify(jobsInfoArray));
+    response.write('</body></html>');
+    
+    
+    //response.write(jobsInfoArray); 
+
+    console.log('Received request to get important job information');
+    response.end();
+});
+
 app.post('/updateJob', function(request, response) {
     if(!request.body) return res.sendStatus(400);
 
