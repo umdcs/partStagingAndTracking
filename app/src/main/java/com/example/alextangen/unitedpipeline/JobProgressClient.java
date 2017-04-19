cd317a45615731221aa0d36aed1f10d8546ae193
Katia Nartovich <narto004@d.umn.edu>
2017-04-19 18:46:27 -0500

    Created a progress bar for each piece in selected job (not functioning - yet).

--- app/src/main/java/com/example/alextangen/unitedpipeline/JobProgressClient.java
+++ app/src/main/java/com/example/alextangen/unitedpipeline/JobProgressClient.java
@@ -2,6 +2,7 @@ package com.example.alextangen.unitedpipeline;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
 
 /**
  * Created by Katia on 4/19/2017.
@@ -10,12 +11,25 @@ import android.support.v7.app.AppCompatActivity;
 public class JobProgressClient extends AppCompatActivity {
 
     GlobalPresenter globs;
+    Job currentJob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_client_view);
+        setContentView(R.layout.activity_job_progress_client);
 
         globs = globs.getInstance();
+
+        String selectionString = getIntent().getStringExtra("selection");
+        int selectionNum = Integer.parseInt(selectionString);
+
+        currentJob = globs.getJobByID(selectionNum);
+
+        ProgressBar[] progArray = new ProgressBar[currentJob.getNumPieces()];
+
+        for (int i = 0; i < currentJob.getNumPieces(); i++) {
+            progArray[i].setProgress(currentJob.getPieceProgress(i));
+        }
+
     }
 }
