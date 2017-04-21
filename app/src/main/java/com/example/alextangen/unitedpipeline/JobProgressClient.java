abcc9922e225dd1ac10a6c155e109cfe386a3afe
Katia Nartovich <narto004@d.umn.edu>
2017-04-21 13:59:14 -0500

    Made progress bars visible!

--- app/src/main/java/com/example/alextangen/unitedpipeline/JobProgressClient.java
+++ app/src/main/java/com/example/alextangen/unitedpipeline/JobProgressClient.java
@@ -2,7 +2,10 @@ package com.example.alextangen.unitedpipeline;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * Created by Katia on 4/19/2017.
@@ -20,16 +23,35 @@ public class JobProgressClient extends AppCompatActivity {
 
         globs = globs.getInstance();
 
-        String selectionString = getIntent().getStringExtra("selection");
-        int selectionNum = Integer.parseInt(selectionString);
+        currentJob = new Job(3);
+        globs.addJob(currentJob);
+        globs.matlRcvd(0, new ProgressBar(this), new TextView(this), 0);
+        globs.matlRcvd(0, new ProgressBar(this), new TextView(this), 1);
+        globs.matlRcvd(0, new ProgressBar(this), new TextView(this), 2);
+        globs.startFab(0, new ProgressBar(this), new TextView(this), 1);
+        globs.startFab(0, new ProgressBar(this), new TextView(this), 2);
+        globs.endFab(0, new ProgressBar(this), new TextView(this), 2);
 
-        currentJob = globs.getJobByID(selectionNum);
+        //String selectionString = getIntent().getStringExtra("selection");
+        //int selectionNum = Integer.parseInt(selectionString);
 
-        ProgressBar[] progArray = new ProgressBar[currentJob.getNumPieces()];
+        //currentJob = globs.getJob(selectionNum);
 
-        for (int i = 0; i < currentJob.getNumPieces(); i++) {
-            progArray[i].setProgress(currentJob.getPieceProgress(i));
-        }
+        //if (currentJob != null) {
+            ProgressBar[] progArray = new ProgressBar[currentJob.getNumPieces()];
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
+
+            for (int i = 0; i < currentJob.getNumPieces(); i++) {
+                progArray[i] = new ProgressBar(this, null, android.R.attr.progressBarStyleHorizontal);
+                progArray[i].setScaleY(3);
+                progArray[i].setIndeterminate(false);
+                progArray[i].setPadding(20,20,20,20);
+                progArray[i].setProgress(currentJob.getPieceProgress(i));
+                System.out.println("Piece progress = " + currentJob.getPieceProgress(i));
+                progArray[i].setVisibility(View.VISIBLE);
+                layout.addView(progArray[i]);
+            }
+        //}
 
     }
 }
