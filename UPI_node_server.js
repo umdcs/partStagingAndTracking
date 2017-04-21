75b886ada78445f42268a6cef30f9835b00eddc5
Alex Tangen <tange045@d.umn.edu>
2017-04-20 19:42:17 -0500

    changes to node server to make it runs correctly

--- UPI_node_server.js
+++ UPI_node_server.js
@@ -1,52 +1,52 @@
 //Basic Node Server for UPI Project
-
+ 
 //Variables:
-
+ 
 var jobsArray = [];
-
-var jobsInfoArray = { 
+ 
+var jobsInfoArray = {
     //'lastUpdated' : 0,
     //'host' : 'UPI',
     'jobArray' : []
 }
-
+ 
 var express = require('express');
-
+ 
 var bodyParser = require('body-parser');
-
+ 
 var app = express();
-
-app.set("port", 8090);
-
+ 
+app.set("port", 3316);
+ 
 app.use(bodyParser.urlencoded({ // support encoded bodies
     extended: true
 }));
-
+ 
 app.use(bodyParser.json()); // support json encoded bodies
-
+ 
 app.get('/', function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'});
-    
+ 
     response.write('<!DOCTYPE html><head><title>Basic Manager Dashboard</title></head><body>');
     response.write('<H1>Manager Dashboard</H1>');
     response.write('JSON Data:');
-    
+ 
     /* You could output any JavaScript data here... */
     response.write(JSON.stringify(jobsArray, null, 4));
     response.write('</body></html>');
-    
+ 
     response.end();
-   
+ 
     console.log('Received dashboard request!');
 });
-
+ 
 app.get('/getAllJobs', function(request, response) {
-    //if(!request.body) return res.sendStatus(400);
+    if(!request.body) return res.sendStatus(400);
     console.log('Received a get request for all jobs!');
-    response.write(jobsArray.length);
+    response.send(jobsArray.length.toString());
     response.end();
 });
-
+ 
 /* getJobByID should take in one paramater, the ID, and search through the jobsInfoArray
    to find find the array element that we wish to find, from there it should take that index
    and grab the job (the same index) from the jobsArray. That data should then be written as
@@ -54,118 +54,118 @@ app.get('/getAllJobs', function(request, response) {
 app.get('/getJobByID/:idNumber', function(request, response) {
     //An ID should be sent up with request, so a specific job can be grabbed
     //Then send back the job corresponding to the ID
-/*
-	for(int i=0; i<jobsInfoArray.length(); i++) {
-	if(jobsInfoArray.getID(i) == (Parameter that comes in)) {
-	response.write(jobsArray[i];
-	OR
-	response.send(jobsArray[i];
-	console.log('Found job')
-	}
-	}
-	response.send('Job not found');
-	response.end();
- */
+    /*
+        for(int i=0; i<jobsInfoArray.length(); i++) {
+        if(jobsInfoArray.getID(i) == (Parameter that comes in)) {
+        response.write(jobsArray[i];
+        OR
+        response.send(jobsArray[i];
+        console.log('Found job')
+        }
+        }
+        response.send('Job not found');
+        response.end();
+    */
     if(!request.body) return res.sendStatus(400);
     var id = request.params.idNumber;
-    response.send(jobsArray[id]); 
+    response.send(jobsArray[id]);
     console.log('Received a get request for a job by ID!');
     response.end();
 });
-
+ 
 /* getJobInfo will be used to send an array with Job ID's and
    the Job names. This will be used to display the current jobs
    that are on the server. Then as the user wants to update or view
    specific jobs, will the job be requested from the server */
-
+ 
 /*
 app.get('/getJobInfo', function(request, response) {
     var jobsInfoArray = [];
     var jobInfo;
-    
+ 
     for(var i = 0; i < jobsArray.length; ++i) {
-	jobInfo = { "jobID" : "", "jobName" : "" };
-	jobsInfoArray.push[jobInfo];
+        jobInfo = { "jobID" : "", "jobName" : "" };
+        jobsInfoArray.push[jobInfo];
     }
-
+ 
     response.send(jobsInfoArray);
     console.log('Received a get request for the names of jobs!');
     response.end();
 });
 */
-
+ 
 app.post('/addJob', function(request, response) {
     if(!request.body) return res.sendStatus(400);
-    
+ 
     var aJob = request.body.Job;
     jobsArray.push(aJob);
-    
+ 
     console.log('Received a post request for adding a job!');
     response.end();
 });
-
-
+ 
+ 
 /* updateJob will be sent a Job to update, and an index
-   where this job is located (to be updated). From this, 
+   where this job is located (to be updated). From this,
    the job can be updated easily without touching other data */
-
+ 
 app.post('/importantInfo', function(request,response) {
     if(!request.body) return response.sendStatus(400);
-
+ 
     //var aJob = request.body.Job;
-
+ 
     //var info = {"name" : request.body.name, "ID" : request.body.ID}
     var info = {"name" : request.body.name, "ID" : request.body.ID};
     jobsInfoArray.jobArray.push(info);
     //lastUpdated++;
-
+ 
     //response.write("Success");
     console.log('Pushed Name and ID onto jobsInfoArray');
     response.end();
 });
-
+ 
 app.get('/getImportantInfo', function(request,response) {
-    /*
+        /*
     response.writeHead(200, {'Content-Type': 'text/html'});
-    
+ 
     response.write('<!DOCTYPE html><head><title>Job Information</title></head><body>');
     response.write('<H1>Names and Numbers</H1>');
     response.write('JSON Data:');
-    
+ 
     /* You could output any JavaScript data here... *
     response.write(JSON.stringify(jobsInfoArray));
     response.write('</body></html>'); */
-    
+ 
     //response.send(jobsInfoArray);
-    //response.write(jobsInfoArray); 
-
+    //response.write(jobsInfoArray);
+ 
     console.log('Received request to get important job information');
     response.json(jobsInfoArray);
     //response.send(jobsInfoArray);
     //response.end();
 });
-
+ 
 app.post('/updateJob', function(request, response) {
     if(!request.body) return res.sendStatus(400);
-
+ 
     var update = request.body.Job;
     var indexToUpdate = request.body.ID;
-
+ 
     jobsArray[indexToUpdate] = update;
-
+ 
     console.log('Received a post request for updating a job!');
     response.end();
 });
-
+ 
 app.use(function(req, res, next) {
     res.status(404).send("Sorry can't find that!");
 });
-
+ 
 app.use(function(err, req, res, next) {
     console.error(err.stack);
     res.status(500).send("Something broke!");
 });
-
+ 
 app.listen(app.get("port"), function () {
     console.log('UPI Node Server: Node app listening on port: ', app.get("port"));
 });
